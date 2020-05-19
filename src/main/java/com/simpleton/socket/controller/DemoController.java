43266c27319ba41c7441e445364fe8570c087883
Harry <harry_myemail@163.com>
2020-05-19 10:48:47 +0800

    .

--- src/main/java/com/simpleton/socket/controller/DemoController.java
+++ src/main/java/com/simpleton/socket/controller/DemoController.java
@@ -1,9 +1,6 @@
 package com.simpleton.socket.controller;
 
-import com.sun.org.apache.xpath.internal.operations.Mod;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
